5defb12b33c7cc82e03c5e412fa4d73e1b1cf8ec
Paula Ashraf <paula.a.pham@gmail.com>
2025-03-13 13:06:09 +0200

    initializa routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'auth' },
+  {
+    path: 'auth',
+    loadChildren: () =>
+      import('./features/auth/auth.routes').then((m) => m.authRoutes),
+  },
+];
--- src/app/features/auth/auth.routes.ts
+++ src/app/features/auth/auth.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { AuthComponent } from './auth.component';
 
-export const authRoutes: Routes = [];
+export const authRoutes: Routes = [
+  {
+    path: '',
+    component: AuthComponent,
+    children: [],
+  },
+];
