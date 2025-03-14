446c670a8d838abe4a0a6b97aabec38eb4ee9321
Paula Ashraf <paula.a.pham@gmail.com>
2025-03-14 20:37:37 +0200

    create user model

---
+++ src/app/shared/models/iuser.ts
@@ -0,0 +1,16 @@
+export interface IUser {
+  id: number;
+  username: string;
+  email: string;
+  firstName: string;
+  lastName: string;
+  gender: string;
+  image: string;
+  accessToken: string;
+  refreshToken: string;
+}
+
+export interface IUserCredentials {
+  username: string;
+  password: string;
+}
