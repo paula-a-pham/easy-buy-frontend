960f5291d6112c42be1551f953c1244aaefb2722
Paula Ashraf <paula.a.pham@gmail.com>
2025-03-16 21:38:49 +0200

    create new method in auth service to get current user

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,6 +1,6 @@
 import { inject, Injectable } from '@angular/core';
 import { environment } from '../../../environments/environment';
-import { IUser, IUserCredentials } from '../../shared/models/iuser';
+import { IBaseUser, IUser, IUserCredentials } from '../../shared/models/iuser';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 
@@ -10,8 +10,13 @@ import { HttpClient } from '@angular/common/http';
 export class AuthService {
   constructor(private httpClient: HttpClient) {}
 
-  login(userCredentials: IUserCredentials): Observable<IUser> {
+  login(userCredentials: IUserCredentials): Observable<IBaseUser> {
     const apiUrl = environment.apiUrl + '/auth/login';
-    return this.httpClient.post<IUser>(apiUrl, userCredentials);
+    return this.httpClient.post<IBaseUser>(apiUrl, userCredentials);
+  }
+
+  getCurrentUser(): Observable<IUser> {
+    const apiUrl = environment.apiUrl + '/auth/me';
+    return this.httpClient.get<IUser>(apiUrl);
   }
 }
