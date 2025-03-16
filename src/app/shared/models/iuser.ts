dbb81833d198cff9bdc39f3fe853b177104f925e
Paula Ashraf <paula.a.pham@gmail.com>
2025-03-16 21:38:44 +0200

    update user interface

--- src/app/shared/models/iuser.ts
+++ src/app/shared/models/iuser.ts
@@ -1,4 +1,9 @@
-export interface IUser {
+export interface IUserCredentials {
+  username: string;
+  password: string;
+}
+
+export interface IBaseUser {
   id: number;
   username: string;
   email: string;
@@ -10,7 +15,82 @@ export interface IUser {
   refreshToken: string;
 }
 
-export interface IUserCredentials {
-  username: string;
+export interface IUser extends Omit<IBaseUser, 'accessToken' | 'refreshToken'> {
+  maidenName: string;
+  age: number;
+  phone: string;
   password: string;
+  birthDate: string;
+  bloodGroup: string;
+  height: number;
+  weight: number;
+  eyeColor: string;
+  hair: Hair;
+  ip: string;
+  address: Address;
+  macAddress: string;
+  university: string;
+  bank: Bank;
+  company: Company;
+  ein: string;
+  ssn: string;
+  userAgent: string;
+  crypto: Crypto;
+  role: string;
+}
+
+export interface Hair {
+  color: string;
+  type: string;
+}
+
+export interface Address {
+  address: string;
+  city: string;
+  state: string;
+  stateCode: string;
+  postalCode: string;
+  coordinates: Coordinates;
+  country: string;
+}
+
+export interface Coordinates {
+  lat: number;
+  lng: number;
+}
+
+export interface Bank {
+  cardExpire: string;
+  cardNumber: string;
+  cardType: string;
+  currency: string;
+  iban: string;
+}
+
+export interface Company {
+  department: string;
+  name: string;
+  title: string;
+  address: Address2;
+}
+
+export interface Address2 {
+  address: string;
+  city: string;
+  state: string;
+  stateCode: string;
+  postalCode: string;
+  coordinates: Coordinates2;
+  country: string;
+}
+
+export interface Coordinates2 {
+  lat: number;
+  lng: number;
+}
+
+export interface Crypto {
+  coin: string;
+  wallet: string;
+  network: string;
 }
