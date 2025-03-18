ce32b61973fe598c99d9a26e6ce3a47e4c5e43af
Paula Ashraf <paula.a.pham@gmail.com>
2025-03-18 11:15:32 +0200

    update user layout

--- src/app/layouts/user-layout/user-layout.component.ts
+++ src/app/layouts/user-layout/user-layout.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from "./components/navbar/navbar.component";
+import { FooterComponent } from "./components/footer/footer.component";
 
 @Component({
   selector: 'app-user-layout',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, FooterComponent],
   templateUrl: './user-layout.component.html',
   styleUrl: './user-layout.component.css'
 })
